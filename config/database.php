d2b9d85bbbf3147f3c6b1c08f523b26daf348dd6
Peter DuCharme <peter.ducharme@coding.partners>
2021-02-16 07:59:15 -0500

    wip DB second connection

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'loomysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('LOOM_DB_HOST', '127.0.0.1'),
+            'port' => env('LOOM_DB_PORT', '3306'),
+            'database' => env('LOOM_DB_DATABASE', 'forge'),
+            'username' => env('LOOM_DB_USERNAME', 'forge'),
+            'password' => env('LOOM_DB_PASSWORD', ''),
+            'unix_socket' => env('LOOM_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
